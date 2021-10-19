01b6e223f1b3cf2863caf902ddb10a3fec692a14
kaischaefer2000 <kai305@web.de>
2021-10-19 14:17:28 +0200

    first commit of the thought collector website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Thought Collector`,
+    description: `Stay organized and get the most out of all your books with the Thought Collector App`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -28,7 +29,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/components/benefits.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const benefits = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default benefits
