0934a531001221b5a2e041d8f7f399f110ea8c38
kaischaefer2000 <kai305@web.de>
2021-10-21 13:08:59 +0200

    fixing document build error 1

--- src/components/header.js
+++ src/components/header.js
@@ -12,135 +12,135 @@ const navigation = [
   { name: "Kontakt", href: "/#contact" },
 ]
 
-const containerWidth = document.querySelector("#___gatsby")
-const screenWidth = containerWidth.offsetWidth
-
-
-const Header = ({ active }) => (
-  <header>
-    <div className="relative overflow-hidden">
-      <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 ">
-          <Popover>
-            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-              <nav
-                className="relative flex items-center justify-between sm:h-10 lg:justify-start"
-                aria-label="Global"
-              >
-                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                  <div className="flex items-center justify-between w-full md:w-auto">
-                    <a href="/">
-                      <span className="sr-only">Workflow</span>
-                      <img className="h-8 w-auto sm:h-10" src={Icon} />
-                    </a>
-                    <div className="-mr-2 flex items-center md:hidden">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Open main menu</span>
-                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
+const Header = ({ active }) => {
+  const containerWidth = document.querySelector("#___gatsby")
+  const screenWidth = containerWidth.offsetWidth
+ 
+  return (
+    <header>
+      <div className="relative overflow-hidden">
+        <div className="max-w-7xl mx-auto">
+          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 ">
+            <Popover>
+              <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+                <nav
+                  className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                  aria-label="Global"
+                >
+                  <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div className="flex items-center justify-between w-full md:w-auto">
+                      <a href="/">
+                        <span className="sr-only">Workflow</span>
+                        <img className="h-8 w-auto sm:h-10" src={Icon} />
+                      </a>
+                      <div className="-mr-2 flex items-center md:hidden">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                          <span className="sr-only">Open main menu</span>
+                          <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                  {navigation.map(item => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="font-medium text-gray-500 hover:text-gray-900"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </nav>
-            </div>
-
-            <Transition
-              as={Fragment}
-              enter="duration-150 ease-out"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="duration-100 ease-in"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Popover.Panel
-                focus
-                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-              >
-                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div className="px-5 pt-4 flex items-center justify-between">
-                    <div>
-                      <img className="h-8 w-auto " src={Icon} alt="" />
-                    </div>
-                    <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Close main menu</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                  <div className="px-2 pt-2 pb-3 space-y-1">
+                  <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
                     {navigation.map(item => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                        className="font-medium text-gray-500 hover:text-gray-900"
                       >
                         {item.name}
                       </a>
                     ))}
                   </div>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
+                </nav>
+              </div>
 
-          <main
-            style={{ display: `${active}` }}
-            className="mt-8 mx-auto max-w-7xl px-4 sm:mt-10 sm:px-6 md:mt-16 lg:mt-18 lg:px-8 xl:mt-24"
-          >
-            <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Thought Collector</span>{" "}
-                <span className="block text-indigo-600 xl:inline">
-                  deine Notiz-App
-                </span>
-              </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-md sm:max-w-xl sm:mx-auto md:mt-5 md:text-md lg:mx-0">
-                Kennst du dieses Problem, wenn du eine Notiz suchst und einfach
-                nicht mehr weist wo du sie in dein Buch geschreiben hast? Also
-                ich schon- und ich hasse es! Deshalb habe ich den Thought
-                Collector entwickelt. Eine{" "}
-                <span className="font-bold">kostenlose</span> Web-App, die dir
-                hilft deine Notizen und Learnings zu organisieren.
-              </p>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <div className="rounded-md shadow">
-                  <a
-                    href="https://thoughtcollector.netlify.app/"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-3 md:text-lg md:px-8"
-                  >
-                    Jetzt kostenlos ausprobieren!
-                  </a>
+              <Transition
+                as={Fragment}
+                enter="duration-150 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Popover.Panel
+                  focus
+                  className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                >
+                  <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div className="px-5 pt-4 flex items-center justify-between">
+                      <div>
+                        <img className="h-8 w-auto " src={Icon} alt="" />
+                      </div>
+                      <div className="-mr-2">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                          <span className="sr-only">Close main menu</span>
+                          <XIcon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                      </div>
+                    </div>
+                    <div className="px-2 pt-2 pb-3 space-y-1">
+                      {navigation.map(item => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
+
+            <main
+              style={{ display: `${active}` }}
+              className="mt-8 mx-auto max-w-7xl px-4 sm:mt-10 sm:px-6 md:mt-16 lg:mt-18 lg:px-8 xl:mt-24"
+            >
+              <div className="sm:text-center lg:text-left">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                  <span className="block xl:inline">Thought Collector</span>{" "}
+                  <span className="block text-indigo-600 xl:inline">
+                    deine Notiz-App
+                  </span>
+                </h1>
+                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-md sm:max-w-xl sm:mx-auto md:mt-5 md:text-md lg:mx-0">
+                  Kennst du dieses Problem, wenn du eine Notiz suchst und
+                  einfach nicht mehr weist wo du sie in dein Buch geschreiben
+                  hast? Also ich schon- und ich hasse es! Deshalb habe ich den
+                  Thought Collector entwickelt. Eine{" "}
+                  <span className="font-bold">kostenlose</span> Web-App, die dir
+                  hilft deine Notizen und Learnings zu organisieren.
+                </p>
+                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                  <div className="rounded-md shadow">
+                    <a
+                      href="https://thoughtcollector.netlify.app/"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-3 md:text-lg md:px-8"
+                    >
+                      Jetzt kostenlos ausprobieren!
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          </main>
-        </div>
-        <div
-          style={{ display: `${active}` }}
-           className="lg:absolute lg:inset-y-0 lg:right-0"
-        >
-          <img
-            className="p-0 lg:p-16  lg:w-full lg:h-full"
-            src={screenWidth < 1000 ? Bg2 : Bg1}
-            alt=""
-          />
+            </main>
+          </div>
+          <div
+            style={{ display: `${active}` }}
+            className="lg:absolute lg:inset-y-0 lg:right-0"
+          >
+            <img
+              className="p-0 lg:p-16  lg:w-full lg:h-full"
+              src={screenWidth < 1000 ? Bg2 : Bg1}
+              alt=""
+            />
+          </div>
         </div>
       </div>
-    </div>
-    {/* <div
+      {/* <div
       style={{
         minHeight: "90vh",
         backgroundImage: `url(${bg1})`,
@@ -149,7 +149,8 @@ const Header = ({ active }) => (
     >
       <h1>hi</h1>
     </div> */}
-  </header>
-)
+    </header>
+  )
+}
 
 export default Header
